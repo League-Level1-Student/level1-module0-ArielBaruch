3d9ad41b96ac144d68528674ad6741431581becc
League Student <league@iMac-37.attlocal.net>
2019-11-19 20:01:12 -0800

    HHR
    :chicken:  mcNuggets

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,63 @@
 package _01_houses;
 
+import java.awt.AWTException;
+
+import javax.swing.JOptionPane;
+
+
+import org.jointheleague.graphical.robot.Robot;
+
+
 public class Houses {
+	Robot r = new Robot();
+	
+	
+	
+	
 	public void run() {
 		
+		r.setX(50);
+		r.setY(540);		
+		r.setSpeed(10);
+		r.setPenColor(150, 0 , 0);
+		r.penDown();
+		r.move(80);
+		r.turn(30);
+		r.move(30);
+		r.turn(125);
+		r.move(30);
+		r.turn(25);
+		r.move(80);
+		r.setPenColor(0, 150, 0);
+		r.turn(-90);
+		r.move(30);
+		r.turn(-90);
+	}
+	public void cool() {
+		r.setPenColor(0,0,40);
+		r.move(200);
+		r.turn(90);
+		r.move(20);
+		r.turn(90);
+		r.move(200);
+		r.setPenColor(0,150,0);
+		r.turn(-90);
+		r.move(30);
+		r.turn(-90);
+	}
+	public void nice() {
+		r.setPenColor(0,0,150);
+		r.move(360);
+		r.turn(30);
+		r.move(30);
+		r.turn(125);
+		r.move(30);
+		r.turn(90);
+		r.move(360);
+		r.setPenColor(0,150,0);
+		r.turn(-90);
+		r.move(30);
+		r.turn(-90);
 	}
 }
+
--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,12 +1,20 @@
 package _01_houses;
 
+import javax.swing.JOptionPane;
+
 public class HousesRunner {
 
 	public static void main(String[] args) {
+		
+
+		JOptionPane.showConfirmDialog(null, "Small, Moderate, Large");
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-
+		Houses house = new Houses();
+		houseBuilder.cool();
+		Houses house1 = new Houses();
+		houseBuilder.nice();
 
 	}
 
